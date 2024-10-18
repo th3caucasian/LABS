69cfa9f6b20e4413ca6b976c27532aadfeda7527
th3caucasian <alan-erkenov@mail.ru>
2024-10-18 03:19:30 +0300

    Появились все города

--- app/src/main/java/com/example/labs/MainActivity2.kt
+++ app/src/main/java/com/example/labs/MainActivity2.kt
@@ -9,20 +9,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.labs.databinding.ActivityMain2Binding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView = binding.navView
-
         navController = findNavController(R.id.nav_host_fragment_activity_main2)
-
         navView.setupWithNavController(navController)
     }
 
--- app/src/main/java/com/example/labs/MyRecycleViewAdapter.kt
+++ app/src/main/java/com/example/labs/MyRecycleViewAdapter.kt
@@ -1,17 +1,16 @@
 package com.example.labs
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.labs.ui.selected.SelectedFragment
 
 class MyRecycleViewAdapter(private val mDataset: Array<String>, private val activity: MainActivity2?): RecyclerView.Adapter<MyRecycleViewAdapter.MyViewHolder>() {
 
     class MyViewHolder(v: View): RecyclerView.ViewHolder(v) {
-        private val mTextView: TextView = v.findViewById(R.id.rec_text_view)
+        val mTextView: TextView = v.findViewById(R.id.rec_text_view)
 
         fun bind(item: String, activity: MainActivity2?) {
             mTextView.text = item
@@ -23,11 +22,13 @@ class MyRecycleViewAdapter(private val mDataset: Array<String>, private val acti
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.recycle_view_item, parent, false)
-        return MyViewHolder(v)
+        val vh = MyViewHolder(v)
+        return vh
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bind(mDataset[position], activity)
+        Log.e("TAG", "${mDataset[1]}, ${mDataset[3]}, ${mDataset[15]}")
     }
 
     override fun getItemCount(): Int {
