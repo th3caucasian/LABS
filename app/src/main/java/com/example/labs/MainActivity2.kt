5ef66d020bd8cd1b1dae300ea4b94cc7d3653b6c
th3caucasian <alan-erkenov@mail.ru>
2024-10-31 14:48:04 +0300

    Готовая 4 лаба

--- app/src/main/java/com/example/labs/MainActivity2.kt
+++ app/src/main/java/com/example/labs/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.labs
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -8,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.labs.databinding.ActivityMain2Binding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -28,7 +30,7 @@ class MainActivity2 : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment_activity_main2)
         navView.setupWithNavController(navController)
 
-        loadJSON()
+        threadManager()
     }
 
     fun onTextViewClicked(city: String) {
@@ -37,9 +39,9 @@ class MainActivity2 : AppCompatActivity() {
         navController.navigate(R.id.navigation_selected, city_name)
     }
 
-    fun loadJSON() {
+    private fun loadJSON() {
         // JSON
-        val url1 = "https://developer.android.com"
+        val url1 = "https://raw.githubusercontent.com/Lpirskaya/JsonLab/master/City2022.json"
         val url: URL? = try {
             URL(url1)
         } catch (e: MalformedURLException) {
@@ -63,14 +65,14 @@ class MainActivity2 : AppCompatActivity() {
             try {
                 if (conn.responseCode == HttpURLConnection.HTTP_OK)
                 {
-                    // smth
+                    Log.e("JSON", "${conn.responseCode}")
                 }
             }
             catch (e: IOException) {
                 e.printStackTrace()
             }
 
-        // Reading JSON
+            // Reading JSON
 
             var response = StringBuilder()
             var input = BufferedReader(InputStreamReader(conn.inputStream), 8192)
@@ -81,8 +83,34 @@ class MainActivity2 : AppCompatActivity() {
                 line = input.readLine()
                 response.append(line)
             } while(line != null)
+
+            val jsonArray = JSONArray(response.toString())
+            for (i in 0..jsonArray.length()-1)
+            {
+                val arrayItem = jsonArray.getJSONObject(i)
+                val population = arrayItem.getString("Population")
+                Log.e("JSON", "Population: ${population}")
+                val country = arrayItem.getString("Country")
+                Log.e("JSON", "Country: ${country}")
+                val name = arrayItem.getString("Name")
+                Log.e("JSON", "Name: ${name}")
+                val language = arrayItem.getString("language")
+                Log.e("JSON", "language: ${language}")
+                val square = arrayItem.getString("square")
+                Log.e("JSON", "square: ${square}")
+            }
         }
 
+
+    }
+
+
+    private fun threadManager() {
+        val thread = Thread(Runnable {
+            loadJSON()
+        })
+
+        thread.start()
     }
 
 }
